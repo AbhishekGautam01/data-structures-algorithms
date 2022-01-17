e9a3e8d848b30f92dbfa3c47056b4d5898808153
AbhishekGautam01 <er.abhishek.gautam@outlook.com>
2022-01-17 18:05:26 +0530

    Linear Search

--- src/algorithms/algorithms/algorithms/searching/Linear.cs
+++ src/algorithms/algorithms/algorithms/searching/Linear.cs
@@ -1,29 +1,4 @@
-# Linear Search 
-* Start from left ost element of arr[] and one by one compare x with each element of arr[]
-* If x matches with an element, return the index. 
-* If x doesn't match with any elements, return -1.  
-
-# Complexity
-1. **Best case time**: O(1)
-2. **Average Case time**: O(N)
-3. **Worst Case time**: O(N)
-4. **Space Complexity**: O(1)
-
-
-# Pseudocode 
-```
-procedure linear_search(list, value)
-    for each item in the list 
-        if match item == value 
-            return the item's location
-        end if 
-    end for
-end procedure
-```
-
-# C# Implementation
-````
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -58,4 +33,3 @@ namespace algorithms.searching
         }
     }
 }
-````
